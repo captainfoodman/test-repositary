8bc739466890411890792011e6173150e1a4054e
= <=>
2022-05-25 10:40:19 +0530

    modified hello.c

--- hello.c
+++ hello.c
@@ -3,4 +3,6 @@ int main()
 {
     printf("hello world!\n");
     printf("this is a new line created by meghu!");
+     printf("this is a new line created by santhosh!");
+     printf("this is a new line created by krishnan!");
 }
